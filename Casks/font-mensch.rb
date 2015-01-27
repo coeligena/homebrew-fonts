98efad20ef991bb497ec6776aadd83b04ae93266
coeligena <coeligenai@gmail.com>
2015-01-27 22:37:25 +0200

    Create font-mensch.rb

---
+++ Casks/font-mensch.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'font-mensch' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://robey.lag.net/downloads/mensch.ttf'
+  homepage 'http://robey.lag.net/2010/06/21/mensch-font.html'
+  license :ofl
+
+  font 'Mensch.ttf'
+end
